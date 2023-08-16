5f37270e071aa50843b5bd55f03ea5a15f4d8aee
Vasavi Simma <simmavasavi1427@gmail.com>
2023-08-16 20:59:07 +0530

    Board class implementation

---
+++ src/models/Board.java
@@ -0,0 +1,48 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    int size;
+    List<List<Cell>> board;
+
+    public Board(int size) {
+        this.size = size;
+        this.board = new ArrayList<>();
+
+        for(int i=0;i<this.size;i++){
+            this.board.add(new ArrayList<>());
+            for(int j=0;j<this.size;j++){
+                this.board.get(j).add(new Cell(i,j));
+            }
+        }
+    }
+
+    public void printBoard(){
+        for(int i=0;i<this.size;i++) {
+            List<Cell> cells = this.board.get(i);
+            for (int j = 0; j < this.size; j++) {
+                cells.get(j).display();
+            }
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public List<List<Cell>> getCells() {
+        return board;
+    }
+
+    public void setCells(List<List<Cell>> cells) {
+        this.board = cells;
+    }
+
+
+}
